5bf1437a901aa6d37524c0d136aed4e81997a8dd
Jean-Philippe Cartoux <CARJ814@retraitequebec.gouv.qc.ca>
2026-02-25 17:16:19 -0500

    base azure helper

--- Readme.md
+++ Readme.md
@@ -1,14 +1,30 @@
 
-[Creer Azure Extension](https://learn.microsoft.com/en-us/azure/devops/extend/get-started/node?view=azure-devops)
+# Pr Markdown Preview
+
+Ajoute un onglet de comparaison des fichiers Markdown modifiés dans un mode Preview.
 
 ## Debug
 
-[Base project](https://github.com/microsoft/azure-devops-extension-hot-reload-and-debug)
+[Projet de base](https://github.com/microsoft/azure-devops-extension-hot-reload-and-debug)
+
+Il faut compiler l'app pour générer le dist/ :
 
 > npx webpack --mode development
 
-Change version number in vss-extension.json then :
+Ensuite vous devez changer le numéro de version dans vss-extension.json puis vous allez pouvoir packager l'extension en mode "dev" :
 
 > npx tfx-cli extension create --manifest-globs vss-extension.json --overrides-file configs/dev.json
 
+Déployez l'extension dans le marketplace, puis vous allez pouvoir lancer l'application localement via :
+
 > npx webpack-dev-server --mode development
+
+Ouvrez dans le navigateur l'url, et ignorer l'alert du certificat car nous sommes en locahost\* :
+
+[](https://localhost:3000/dist/PrMarkdownPreview/PrMarkdownPreview.html)
+
+Vous avez à approuver une notification de votre navigateur, car du contexte du site https://dev.azure.com/ notre extension souhaite communiquer avec le localhost :
+
+![Azure site notification](./img/debug-az-ext.png)
+
+\* Si vous avez un soucis comme quoi l'extension met du temps à charger refaite cette étape !
--- src/PrMarkdownPreview/PrMarkdownPreview.tsx
+++ src/PrMarkdownPreview/PrMarkdownPreview.tsx
@@ -15,17 +15,233 @@ interface IPrMarkdownPreviewState {
   panelShown: boolean;
 }
 
+class AzureAPIHelper {
+  organization!: string;
+  project!: string;
+  repositoryId!: string;
+
+  public init(
+    organization: string,
+    project: string,
+    repositoryId: string) 
+  {
+    this.organization = organization;
+    this.project = project;
+    this.repositoryId = repositoryId;
+  }
+
+  private async PrepareHeaders()
+  {
+    const token = await SDK.getAccessToken();
+    const headers = {
+      "Authorization": `Bearer ${token}`,
+      "Content-Type": "application/json"
+    };
+
+    return headers;
+  }
+
+  public async GetCommits(
+    prId: string
+  )
+  {
+    const u = new URL(`https://dev.azure.com/${this.organization}/${this.project}/_apis/git/repositories/${this.repositoryId}/pullRequests/${prId}/commits`);
+    u.searchParams.set("api-version", "7.1");
+
+    const headers = await this.PrepareHeaders();
+
+    const response = await fetch(u, { headers });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    const commits = 
+      data.value.map(
+        (commit: Commit) => commit);
+    
+    return commits;
+  }
+
+  public async GetFilesChanges(
+    commitId: string
+  )
+  {
+    const u = new URL(`https://dev.azure.com/${this.organization}/${this.project}/_apis/git/repositories/${this.repositoryId}/commits/${commitId}/changes`);
+    u.searchParams.set("api-version", "7.1");
+
+    const headers = await this.PrepareHeaders();
+
+    const response = await fetch(u, { headers });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    const changes = 
+      data.changes.map(
+        (change: Change) => change);
+    
+    return changes;
+  }
+
+  public async GetFileContent(
+    filePath: string,
+    commitId: string
+  )
+  {
+    const u = new URL(`https://dev.azure.com/${this.organization}/${this.project}/_apis/git/repositories/${this.repositoryId}/items`);
+    u.searchParams.set("api-version", "7.1");
+    u.searchParams.set("path", filePath);
+    u.searchParams.set("versionDescriptor.version", commitId);
+    u.searchParams.set("versionDescriptor.versionType", "commit");
+    u.searchParams.set("includeContent", "true");
+
+    const headers = await this.PrepareHeaders();
+
+    const response = await fetch(u, { headers });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return await response.text();
+  }
+
+  public async GetDiffs(
+    baseCommit: string, 
+    targetCommit: string)
+  {
+    const u = new URL(`https://dev.azure.com/${this.organization}/${this.project}/_apis/git/repositories/${this.repositoryId}/diffs/commits`);
+    u.searchParams.set("api-version", "7.1");
+    u.searchParams.set("baseVersion", baseCommit);
+    u.searchParams.set("baseVersionType", "commit");
+    u.searchParams.set("targetVersion", targetCommit);
+    u.searchParams.set("targetVersionType", "commit");
+
+    const headers = await this.PrepareHeaders();
+
+    const response = await fetch(u, { headers });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    
+    const changedFiles = 
+      data.changes.map(
+        (change: { item: { path: any; }; }) => change.item.path);
+    
+    return changedFiles;
+  }
+}
+
+class AzurePrConfig {
+  project!: string;
+  repositoryId!: string;
+  prId!: string;
+  mrgCommit!: string;
+  srcCommit!: string;
+  tgtCommit!: string;
+  organization!: string;
+}
+
+class Commit {
+  author!: AuthorInfo;
+  comment!:string; 
+  commitId!:string;
+  committer!: AuthorInfo;
+}
+
+class Entry {
+
+}
+
+class Change {
+  changeType!: string;
+  item!: Item;
+}
+
+class Item {
+  commitId!: string;
+  gitObjectType!: string;
+  objectId!: string;
+  originalObjectId!: string;
+  path!: string;
+  url!: string;
+  content!: string;
+}
+
+class AuthorInfo { 
+  name!:string; 
+  email!:string; 
+  date!:string 
+}
+
 class PrMarkdownPreview extends React.Component<{}, IPrMarkdownPreviewState> {
+  azureAPIHelper: AzureAPIHelper;
+  azurePrConfig: AzurePrConfig;
+
   constructor(props: {}) {
     super(props);
     this.state = {
       dialogShown: false,
       panelShown: false
     };
+    this.azureAPIHelper = new AzureAPIHelper();
+    this.azurePrConfig = new AzurePrConfig();
   }
 
-  public componentDidMount() {
-    SDK.init();
+  public async componentDidMount() {
+    await SDK.init();
+
+    debugger;
+
+    const pageCtx = SDK.getPageContext();
+    this.azurePrConfig.project = pageCtx.webContext.project.name;
+
+    const cfg = SDK.getConfiguration(); 
+
+    this.azurePrConfig.repositoryId = cfg.repositoryId;
+
+    const pr = cfg.pullRequest;
+    this.azurePrConfig.prId = pr.pullRequestId;
+    this.azurePrConfig.mrgCommit = pr.lastMergeCommitId; 
+    this.azurePrConfig.srcCommit = pr.lastMergeSourceCommitId; 
+    this.azurePrConfig.tgtCommit = pr.lastMergeTargetCommitId;
+
+    const host = SDK.getHost();
+    this.azurePrConfig.organization = host.name;
+
+    this.azureAPIHelper.init(
+      this.azurePrConfig.organization, 
+      this.azurePrConfig.project, 
+      this.azurePrConfig.repositoryId);
+
+    let filesSrcChgs = await this.azureAPIHelper.GetFilesChanges(
+      this.azurePrConfig.srcCommit);
+
+    filesSrcChgs.forEach(async (change: Change) => {
+      change.item.content = await this.azureAPIHelper.GetFileContent(
+        change.item.path,
+        change.item.commitId);
+    });
+      
+    let filesTgtChgs = await this.azureAPIHelper.GetFilesChanges(
+      this.azurePrConfig.tgtCommit);
+
+    filesTgtChgs.forEach(async (change: Change) => {
+      change.item.content = await this.azureAPIHelper.GetFileContent(
+        change.item.path,
+        change.item.commitId);
+    });
+
+    console.log(filesSrcChgs);
   }
 
   public render(): JSX.Element {
